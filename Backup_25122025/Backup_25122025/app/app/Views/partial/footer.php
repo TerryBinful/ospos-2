e1fb7a8fea9c50b2791d13851546185ec977865e
TerryBinful <kwakubinful@gmail.com>
2025-12-25 22:56:41 +0000

    did some changes

---
+++ Backup_25122025/Backup_25122025/app/app/Views/partial/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+use Config\OSPOS;
+
+?>
+
+        </div>
+    </div>
+
+	<footer class="d-flex justify-content-center flex-shrink-0 text-center">
+        <div class="footer container-fluid bg-body rounded shadow p-3 mb-md-4 mx-md-3">
+            <span class="text-primary">
+                
+            <span>Powered by FlipStudios🚀</span>
+        </div>
+    </footer>
+    
+        </div>
+    </div>
+</body>
+
+</html>
