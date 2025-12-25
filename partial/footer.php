f86be34bfcfc87fde03d199e07d5a4519b6d3f35
TerryBinful <kwakubinful@gmail.com>
2025-12-25 18:02:53 +0000

    pusing needed files

---
+++ partial/footer.php
@@ -0,0 +1,19 @@
+<?php
+
+use Config\OSPOS;
+
+?>
+
+        </div>
+    </div>
+
+    <div id="footer">
+        <div id="footer_custom">
+            <p class="footer-branding">
+            Powered by <span class="brand-name">FlipStudios 🚀</span>
+        </p>
+        </div>
+    </div>
+</body>
+
+</html>
